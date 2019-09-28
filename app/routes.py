e83fa8b8dfedb47a4b115ba692331ed7471100d2
Jamie Edgecombe <jamie.edgecombe@deliveroo.co.uk>
2019-09-28 21:12:34 +0100

    m

--- app/routes.py
+++ app/routes.py
@@ -83,4 +83,4 @@ def update_account():
     if form.validate_on_submit():
         user = shipmate.query.filter_by(nickname=form.username.data).first()
         user.set_password(form.password.data)
-    return render_template('update_account.html', title='My account')
+        return render_template('update_account.html', title='My account', form=form, user=user)
