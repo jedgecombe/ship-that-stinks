7cfd0a6eebf6f2c6644dd826d5082aef886cef98
Marv <j_marvin@mail.com>
2019-09-28 11:06:28 +0100

    here ya go mayte

---
+++ local-testing/app/models.py
@@ -0,0 +1,58 @@
+from app import db
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return shipmate.query.get(int(id))
+
+class shipmate(db.Model, UserMixin):
+
+    __tablename__ = "shipmates"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True, nullable=False)
+    surname = db.Column(db.String(64), index=True, nullable=False)
+    nickname = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    proposals = db.relationship('event', backref='organiser', lazy='dynamic')
+    responses = db.relationship('proposal_response', backref='responder', lazy='dynamic')
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        return self.password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class proposal_response(db.Model):
+
+    __tablename__ = "proposal_response"
+
+    id = db.Column(db.Integer, primary_key=True)
+    response_datetime = db.Column(db.DateTime, index=True, nullable=False, default=datetime.datetime.now)
+    response = db.Column(db.String(32), index=True, nullable=False)
+    response_status = db.Column(db.String(32), index=True, default="Open")
+    event = db.Column(db.Integer, db.ForeignKey('events.id'))
+    shipmate = db.Column(db.Integer, db.ForeignKey('shipmates.id'))
+
+class event(db.Model):
+
+    __tablename__ = "events"
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(64), index=True)
+    event_date = db.Column(db.Date, index=True, nullable=False)
+    event_time = db.Column(db.Time, index=True, nullable=False)
+    event_location = db.Column(db.String(128))
+    event_status = db.Column(db.String(32),index=True, nullable=False, default="Open")
+    created_at = db.Column(db.DateTime, index=True, default=datetime.datetime.now().date)
+    organised_by = db.Column(db.Integer, db.ForeignKey('shipmates.id'))
+    responses = db.relationship('proposal_response', backref='event_proposal', lazy='dynamic')
+
+
+#    def __repr__(self):
+#        return '<User {}>'.format(self.username)
---
+++ local-testing/app/routes.py
@@ -0,0 +1,70 @@
+from app import app, db
+from flask import render_template, flash, redirect, request, url_for
+from app.forms import EventForm, LoginForm, ResponseForm
+from app.models import event, shipmate, proposal_response
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+import datetime
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = shipmate.query.filter_by(nickname=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/response', methods=['GET', 'POST'])
+@login_required
+def response():
+    if request.args.get('modify'):
+        proposal_response.query.filter_by(id = request.args.get('response_id')).update(dict(response_status="Closed"))
+    focus_event = event.query.get(request.args['event_id'])
+    form = ResponseForm()
+    if form.validate_on_submit():
+        flash("You've responded to the event '{}'".format(focus_event.event_name))
+        new_response = proposal_response(response=form.response.data, event=focus_event.id, shipmate=current_user.id)
+        db.session.add(new_response)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('response.html', title='Respond to an event proposal', event=focus_event, form=form)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    events = event.query.filter_by(event_status = "Open").order_by(event.event_date).all()
+#    proposals = user.proposals.all()
+    return render_template('index.html', title='Home', events=events)
+
+@app.route('/create_event', methods=['GET', 'POST'])
+def create_event():
+    if request.args.get('modify'):
+        focus_event = event.query.get(request.args.get('event_id'))
+        form = EventForm(obj=focus_event)
+        event.query.filter_by(id = focus_event.id).update(dict(event_status="Closed"))
+    else:
+        form = EventForm()
+    if form.validate_on_submit():
+        print(type(form.event_date.data), form.event_date.data)
+        print(type(form.event_time.data), form.event_time.data)
+        flash("Event proposal for '{}' sent".format(form.event_name.data))
+        new_event = event(event_name = form.event_name.data, event_date = form.event_date.data, event_time = form.event_time.data, event_location = form.event_location.data, organised_by=current_user.id)
+        db.session.add(new_event)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create_event.html', title='Create an event', form=form)
